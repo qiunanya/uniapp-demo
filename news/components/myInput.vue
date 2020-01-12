36dc7334706ccc725ab26b4a711630afb3441309
qiunanya <1766226354@qq.com>
2020-01-12 13:01:40 +0800

    子父组件相互调用

---
+++ news/components/myInput.vue
@@ -0,0 +1,48 @@
+<template name='my-input'>
+	<view>
+		<input v-model="age" type="text" value="" :placeholder="placeholder" class="my-input"/>
+		<button type="primary" @click="submitForm">提交</button>
+	</view>
+</template>
+
+<script>
+	export default {
+		name:'my-input',
+		data() {
+			return {
+				age:'',
+			};
+		},
+		props:{
+			placeholder:{
+				type:String,
+		        default:"请输入姓名..."
+			},
+		},
+		created() {
+			console.log('created')
+		},
+		watch:{},
+		methods:{
+			submitForm(){
+				this.$emit('getValue',this.age)
+			},
+			setValue(val){
+				this.age = val;
+			}
+		},
+		computed:{
+			
+		},
+		mounted() {
+			console.log('mounted')
+		}
+	}
+</script>
+
+<style lang="less" scoped>
+.my-input{
+	padding: 20rpx;
+	background: #F4F5F6;
+}
+</style>
--- news/pages/index/index.vue
+++ news/pages/index/index.vue
@@ -1,6 +1,12 @@
 <template>
 	<view class="content">
-		<view class="uni-list">
+		
+		<!-- 自定义组件使用 -->
+		<text style="font-size: 40rpx;color: #F76260;">1.自定义组件myInput</text>
+		<myinput placeholder="请输入年龄" @getValue="getName" ref='myinput'></myinput>
+		
+		<!-- 新闻列表 -->
+		<!-- <view class="uni-list">
 			<view @tap="goNewInfo" :data-newsid='item.post_id' class="uni-list-cell" hover-class="uni-list-cell-hover" 
 			v-for="(item,index) in newList" :key="index">
 				<view class="uni-media-list">
@@ -11,18 +17,27 @@
 					</view>
 				</view>
 			</view>
-		</view>
+		</view> -->
+	
+	
+	
 	</view>
 </template>
 
 <script>
+	import myinput from '../../components/myInput.vue'
 	export default {
+		components:{
+			myinput
+		},
 		data() {
 			return {
 				newList:[]
 			}
 		},
 		onLoad() {
+		  const val = '父组件传来的初始化值'
+		  this.$refs.myinput.setValue(val);	
           uni.request({
           	url: 'https://unidemo.dcloud.net.cn/api/news',
           	method: 'GET',
@@ -36,6 +51,9 @@
           });
 		},
 		methods: {
+		 getName(val){
+			console.log('子组件传值过来了---'+val) 
+		 },	
           goNewInfo(e){
 			let newid = e.currentTarget.dataset.newsid;  
 			uni.navigateTo({
